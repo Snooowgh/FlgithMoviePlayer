880af1d7a2da27c8923d040644e03288bdef5c73
Snooowgh <sustechapp@qq.com>
2018-05-04 11:11:32 +0800

    first commitasdasdasd

---
+++ src/sample/Main.java
@@ -0,0 +1,38 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+        String MovieSource_Path = "./MovieSource";
+        File f = new File(MovieSource_Path);
+        if (f.isDirectory()){
+            File[] fs = f.listFiles();
+            for (int i = 0; i < fs.length; i++) {
+                System.out.println(fs[i].getName());
+            }
+        }else{
+            f.mkdir();
+        }
+    }
+}
